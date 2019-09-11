582a2ac05d6e7d7cb0e5dd414d992eacf4668295
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-09-12 02:34:34 +0300

    Create about.php

---
+++ equipment/about.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<link href="http://localhost/ironhouse/equipment/css/style.css" rel="stylesheet" type="text/css" />
+	<title>ООО"IRON HOUSE"</title>
+</head>
+<body id="container">
+<div id="block-body">
+<?php 
+    include("include/block-header.php");
+    include("admin/inc/count.php");
+?>
+<div id="block-content">
+<h1 align="center" id="titles">О компании</h1>
+<div id="about">
+<p class="style-title-gridOP">Компания Iron House ООО "Стальной Дом" специализируется на создании эффективных автоматизированных систем и комплексных поставках высокотехнологичного оборудования ведущих отечественных и зарубежных производителей.</p>
+<p class="style-title-gridOP">Производство и монтаж парковочного оборудования,ворот,турникетов.</p>
+<p class="style-title-grid">Наши услуги:</p>
+<ul class="style-title-gridOP">
+<li>Производство оборудования для автоматизации парковок</li>
+<li>Мы производим оборудование для автоматизированных парковок и занимаемся разработкой программного обеспечения с учетом индивидуальных требований заказчика.</li>
+</ul>
+<p class="style-title-grid">Проектные работы:</p>
+<p class="style-title-gridOP">
+Мы осуществляем разработку новых проектов с учетом специфических целей использования системы, оказываем консультационные услуги, определяем наилучшее решение для организации парковки.
+</p>
+<p class="style-title-grid">Шеф-монтаж оборудования:</p>
+<p class="style-title-gridOP">Наши опытные сотрудники произведут инсталляцию оборудования, проведут работы по наладке и запуску системы.</p>
+<p class="style-title-gridOP">Кроме установки оборудования АП-ПРО мы оказываем услуги по монтажу оборудования любых других производителей.</p>
+<p class="style-title-grid">Сервисное обслуживание:</p>
+<p class="style-title-gridOP">Мы обеспечиваем качественный сервис и поддержку программного обеспечения в течение всего периода эксплуатации нашей системы. Мы также осуществляем последующую доработку проекта и установку дополнительных модулей по требованию заказчика.</p>
+<p class="style-title-grid" align="center">Вы можете расчитовать на нас, как на надежного партнера. А мы, в свою очередь, будем рады сотрудничеству с Вами.</p>
+<p class="style-title-grid">Наши реквизиты:</p>
+<p class="style-title-gridOP">КПП 781401001 &nbsp;&nbsp;&nbsp;&nbsp; ИНН 7814260265</p>
+</div>
+</div>
+<?php
+    include("include/block-footer.php");
+?>
+</div>
+</body>
+</html>
