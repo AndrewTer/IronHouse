7cd93dfe26c4dec428f974371a596700e29c9313
AndrewCTer <33009070+AndrewTer@users.noreply.github.com>
2019-09-12 03:09:08 +0300

    Create db_connect.php

---
+++ equipment/admin/include/db_connect.php
@@ -0,0 +1,10 @@
+<?
+   define('ironhouse', true);
+   $db_host = 'localhost';
+   $db_user = 'root'; 
+   $db_pass = '';
+   $db_database = 'ironhouse';
+   $connection = mysql_connect($db_host, $db_user, $db_pass);
+   mysql_select_db($db_database,$connection) or die("БД не подключена".mysql_error());
+   mysql_query("set names cp1251");
+?>
